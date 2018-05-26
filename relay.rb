48391315d7292ac006c582f12f7fa26d9b079f1b
Karolis Rusenas <karolis.rusenas@gmail.com>
2018-05-26 18:28:31 +0100

    updated current version sha

--- relay.rb
+++ relay.rb
@@ -2,7 +2,7 @@ class Relay < Formula
   desc "Client for Webhook Relay service - demo websites from localhost and receive webhooks behind firewalls and NAT"
   homepage "https://webhookrelay.com"
   url "https://storage.googleapis.com/webhookrelay/downloads/relay-darwin-amd64"
-  sha256 "d891366f0f137427caabab2fb0b26451b56eb45aee02a5c37beabea8bcf640a3"
+  sha256 "ae3c916c257ef2a678defb00b520580b48ac8cbe1635eb5dab8c1ebe1af1f1f2"
 
   def install    
     mv "relay-darwin-amd64", "relay"
