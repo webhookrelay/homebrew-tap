2da3a17a7847424394a44fde46eb31bb61678a64
Karolis <karolis.rusenas@gmail.com>
2019-01-30 22:20:06 +0000

    Add relay.rb

--- relay.rb
+++ relay.rb
@@ -2,7 +2,7 @@ class Relay < Formula
   desc "Client for Webhook Relay service - demo websites from localhost and receive webhooks behind firewalls and NAT"
   homepage "https://webhookrelay.com"
   url "https://storage.googleapis.com/webhookrelay/downloads/relay-darwin-amd64"
-  sha256 "64af533af519865992ed670e4dfd0a16c01e402a6a3655ed4a640b53752c6892"
+  sha256 "a1aa70f4eaa063d636bb6e598f0a5d8b844842648289c35f2ada9a9a5676e202"
 
   def install    
     mv "relay-darwin-amd64", "relay"
